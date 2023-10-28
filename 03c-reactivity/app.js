0f682c191a0ccdae1d649329ea39c9c8b82dfae0
JarekSopinski <jarek.sopinski@gmail.com>
2023-10-28 13:22:58 +0200

    Watchers

--- 03c-reactivity/app.js
+++ 03c-reactivity/app.js
@@ -2,17 +2,30 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullname: ''
     };
+  },
+  watch: {
+    // watcher methods execute whenever their connected data values changes
+    // last value is always passed as an argument
+    // we can also receive two args (newValue and oldValue)
+    name(value) {
+      // will execute whenever 'name' changes (in data)
+      if (value === '') { this.fullname = ''; }
+      else {
+        this.fullname = value + ' ' + 'Schwarzmuller';
+      }
+    }
   },
   computed: {
     // Computet props are only revaluated if one of their deps. changes
-    fullname() {
-      if (this.name === '') {
-        return '';
-      }
-      return this.name + ' ' + 'Schwarzmuller';
-    }
+    // fullname() {
+    //   if (this.name === '') {
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Schwarzmuller';
+    // }
   },
   methods: {
     setName(event) {
