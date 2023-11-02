7203ebcd5b274b4190f51ae881c5902eebbf3698
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-02 10:05:05 +0100

    Updating health bars

--- 04-monster-game/app.js
+++ 04-monster-game/app.js
@@ -4,27 +4,35 @@ const MONSTER_MIN_ATTACK = 8;
 const MONSTER_MAX_ATTACK = 15;
 
 const getRandomValue = (min, max) => {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 const app = Vue.createApp({
-    data() {
-        return {
-            playerHealth: 100,
-            monsterHealth: 100
-        }
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: `${this.monsterHealth}%` };
     },
-    methods: {
-        attackMonster() {
-            const attackValue = getRandomValue(PLAYER_MIN_ATTACK, PLAYER_MAX_ATTACK);
-            this.monsterHealth -= attackValue;
-            this.attackPlayer();
-        },
-        attackPlayer() {
-            const attackValue = getRandomValue(MONSTER_MIN_ATTACK, MONSTER_MAX_ATTACK);
-            this.playerHealth -= attackValue;
-        }
-    }
+    playerBarStyles() {
+      return { width: `${this.playerHealth}%` };
+    },
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = getRandomValue(PLAYER_MIN_ATTACK, PLAYER_MAX_ATTACK);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(MONSTER_MIN_ATTACK, MONSTER_MAX_ATTACK);
+      this.playerHealth -= attackValue;
+    },
+  },
 });
 
 app.mount('#game');
