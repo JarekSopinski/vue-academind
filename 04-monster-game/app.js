7c321fe73253e6cc8b580b87445dd14ee24e71fb
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-02 10:27:55 +0100

    Adding a special attack

--- 04-monster-game/app.js
+++ 04-monster-game/app.js
@@ -2,6 +2,8 @@ const PLAYER_MIN_ATTACK = 5;
 const PLAYER_MAX_ATTACK = 12;
 const MONSTER_MIN_ATTACK = 8;
 const MONSTER_MAX_ATTACK = 15;
+const SPECIAL_MIN_ATTACK = 10;
+const SPECIAL_MAX_ATTACK = 25;
 
 const getRandomValue = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -12,6 +14,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0
     };
   },
   computed: {
@@ -21,9 +24,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: `${this.playerHealth}%` };
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    }
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(PLAYER_MIN_ATTACK, PLAYER_MAX_ATTACK);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -32,6 +39,12 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(MONSTER_MIN_ATTACK, MONSTER_MAX_ATTACK);
       this.playerHealth -= attackValue;
     },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(SPECIAL_MIN_ATTACK, SPECIAL_MAX_ATTACK);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    }
   },
 });
 
