722d2d6d76a58ccd3e7f0eb82aeaea05a8ee774e
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-02 10:54:18 +0100

    Adding start new game functionality

--- 04-monster-game/app.js
+++ 04-monster-game/app.js
@@ -22,9 +22,15 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: `${this.monsterHealth}%` };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: `${this.playerHealth}%` };
     },
     mayUseSpecialAttack() {
@@ -48,6 +54,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(PLAYER_MIN_ATTACK, PLAYER_MAX_ATTACK);
