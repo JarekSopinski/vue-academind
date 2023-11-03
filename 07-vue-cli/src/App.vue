cebc6b8ed5011072f80fb4863a01c354fd8786fd
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-03 16:05:06 +0100

    Basic app with CLI

---
+++ 07-vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li v-for="f in friends" :key="f.id">{{ f.name }}</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                friends: [
+                    {
+                        id: 'manuel',
+                        name: 'Manuel Lorenz',
+                        phone: '0123 4567 890',
+                        email: 'manuel@localhost.com'
+                    },
+                    {
+                        id: 'julie',
+                        name: 'Julie Jones',
+                        phone: '0123 4567 890',
+                        email: 'julie@localhost.com'
+                    }
+                ]
+            }
+        }
+    }
+</script>
