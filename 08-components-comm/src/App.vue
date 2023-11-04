79b7c70191764653918f27b73b9d76bcb2cf427b
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-04 17:59:06 +0100

    Emitting custom events

--- 08-components-comm/src/App.vue
+++ 08-components-comm/src/App.vue
@@ -7,10 +7,12 @@
       <friend-contact 
         v-for="friend in friends"
         :key="friend.id"
+        :id="friend.id"
         :name="friend.name" 
         :phone-number="friend.phone"
         :email-address="friend.email"
-        :is-favorite="true"
+        :is-favorite="friend.isFavorite"
+        @on-toggle-favorite="toggleFavoriteStatus"
       />
     </ul>
   </section>
@@ -26,16 +28,24 @@ export default {
           name: "Manuel Lorenz",
           phone: "0123 45678 90",
           email: "manuel@localhost.com",
+          isFavorite: true
         },
         {
           id: "julie",
           name: "Julie Jones",
           phone: "0987 654421 21",
           email: "julie@localhost.com",
+          isFavorite: false
         },
       ],
     };
   },
+  methods: {
+    toggleFavoriteStatus(friendId){
+      const foundFriend = this.friends.find(f => f.id === friendId);
+      foundFriend.isFavorite = !foundFriend.isFavorite;
+    }
+  }
 };
 </script>
 
