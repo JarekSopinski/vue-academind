bb2ea14d9ceaa5a3244cdbf1720901ac84431eba
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-04 19:45:40 +0100

    Adding components and connecting them

--- 08-components-comm/src/App.vue
+++ 08-components-comm/src/App.vue
@@ -3,17 +3,11 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @on-add-contact="addContact" />
     <ul>
-      <friend-contact 
-        v-for="friend in friends"
-        :key="friend.id"
-        :id="friend.id"
-        :name="friend.name" 
-        :phone-number="friend.phone"
-        :email-address="friend.email"
-        :is-favorite="friend.isFavorite"
-        @on-toggle-favorite="toggleFavoriteStatus"
-      />
+      <friend-contact v-for="friend in friends" :key="friend.id" :id="friend.id" :name="friend.name"
+        :phone-number="friend.phone" :email-address="friend.email" :is-favorite="friend.isFavorite"
+        @on-toggle-favorite="toggleFavoriteStatus" />
     </ul>
   </section>
 </template>
@@ -41,11 +35,21 @@ export default {
     };
   },
   methods: {
-    toggleFavoriteStatus(friendId){
+    toggleFavoriteStatus(friendId) {
       const foundFriend = this.friends.find(f => f.id === friendId);
       foundFriend.isFavorite = !foundFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name,
+        phone,
+        email,
+        isFavorite: false
+      };
+      this.friends.push(newFriendContact);
     }
-  }
+  },
 };
 </script>
 
@@ -80,7 +84,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -112,5 +117,21 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+
+#app form div {
+  margin: 1rem 0;
 }
 </style>
