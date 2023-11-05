9aa078f7de377f9f2ea5bb994796823f198a6db2
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-05 09:39:54 +0100

    LearningResource component

--- 10-project-learning-app/src/App.vue
+++ 10-project-learning-app/src/App.vue
@@ -1,10 +1,22 @@
 <template>
     <ul>
+        <LearningResource
+            v-for="res in storedResources"
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        />
     </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+    components: {
+        LearningResource
+    },
     data() {
         return {
             storedResources: [
