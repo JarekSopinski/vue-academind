d0c00486fb633673c32d7a83621d2e54492bc813
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-05 06:39:53 +0100

    Style fix

--- 09-components-adv/src/components/UserInfo.vue
+++ 09-components-adv/src/components/UserInfo.vue
@@ -21,12 +21,3 @@ export default {
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
-
-<style scoped>
-/* scoped - styles will be available only for this component */
-section header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-</style>
