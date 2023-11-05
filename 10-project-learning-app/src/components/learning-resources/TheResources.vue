c9b607775d9a09d45b7f6feaa390d25dfab501d7
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-05 10:29:21 +0100

    AddResource, TheResources

---
+++ 10-project-learning-app/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,34 @@
+<template>
+
+    <BaseCard>
+        <BaseButton @click="setSelectedTab('stored-resources')">Stored Resources</BaseButton>
+        <BaseButton @click="setSelectedTab('add-resource')">Add Resource</BaseButton>
+    </BaseCard>
+
+    <component :is="selectedTab">
+
+    </component>
+
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+    components: {
+        StoredResources,
+        AddResource
+    },
+    data() {
+        return {
+            selectedTab: 'stored-resources'
+        }
+    },
+    methods: {
+        setSelectedTab(tab) {
+            this.selectedTab = tab;
+        }
+    }
+}
+</script>
