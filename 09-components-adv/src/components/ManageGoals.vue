54c4cbe2db30342cbbeedcba58d8af2b9db5883b
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-05 08:02:40 +0100

    Solution - teleporting components

--- 09-components-adv/src/components/ManageGoals.vue
+++ 09-components-adv/src/components/ManageGoals.vue
@@ -3,11 +3,16 @@
         <h2>Manage Goals</h2>
         <input type="text" ref="goal">
         <button @click="setGoal">Set Goal</button>
-        <error-alert v-if="inputIsInvalid">
-            <h2>Input is invalid!</h2>
-            <p>Please enter some text...</p>
-            <button @click="confirmError">Okay</button>
-        </error-alert>
+
+        <!-- Teleport renders element in another place in DOM based on provided CSS selector -->
+        <teleport to="body">
+            <error-alert v-if="inputIsInvalid">
+                <h2>Input is invalid!</h2>
+                <p>Please enter some text...</p>
+                <button @click="confirmError">Okay</button>
+            </error-alert>
+        </teleport>
+
     </div>
 </template>
 
@@ -33,6 +38,6 @@ export default {
         confirmError() {
             this.inputIsInvalid = false;
         }
-    } 
+    }
 }
 </script>
