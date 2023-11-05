8cf16bd801844cd048ca6594a258cca038194f7c
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-05 07:27:13 +0100

    Scoped slots

--- 09-components-adv/src/App.vue
+++ 09-components-adv/src/App.vue
@@ -8,6 +8,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+      </template>
+    </course-goals>
+
   </div>
 </template>
 
@@ -15,6 +22,7 @@
 import TheHeader from './components/TheHeader.vue';
 import BadgeList from './components/BadgeList.vue';
 import UserInfo from './components/UserInfo.vue';
+import CourseGoals from './components/CourseGoals.vue';
 
 export default {
   // Components registered here are only available in this component
@@ -23,8 +31,9 @@ export default {
     // TheHeader: TheHeader
     TheHeader,
     BadgeList,
-    UserInfo
-  },
+    UserInfo,
+    CourseGoals
+},
   data() {
     return {
       activeUser: {
