88e3cd7ac1ff98bf4f9e945f39c90ffb8c13f1e7
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-05 06:48:09 +0100

    Default slot content

--- 09-components-adv/src/components/BaseCard.vue
+++ 09-components-adv/src/components/BaseCard.vue
@@ -3,7 +3,10 @@
     <!-- we can use named slot if we want multiple -->
     <div>
         <header>
-            <slot name="header"></slot>
+            <slot name="header">
+                <!-- default slot's content -->
+                <h2>The Default</h2>
+            </slot>
         </header>
         <slot></slot>
     </div>
@@ -11,21 +14,22 @@
 
 <script>
 export default {
-
+    
 }
 </script>
 
 <style scoped>
 header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
+
 div {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
+    margin: 2rem auto;
+    max-width: 30rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
 }
 </style>
