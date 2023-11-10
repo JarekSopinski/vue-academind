463c3c39b1075dc0ce4d15740aa5aee5ba713498
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-10 21:09:01 +0100

    Loading state

--- 12-http-requests/src/components/survey/UserExperiences.vue
+++ 12-http-requests/src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -27,16 +28,19 @@ export default {
   },
   data() {
     return {
-      results: []
+      results: [],
+      isLoading: false
     }
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch(`${FIREBASE_URL}/surveys.json`).then((response) => {
         if (response.ok) {
           return response.json();
         }
       }).then(data => {
+        this.isLoading = false;
         console.log(data);
         const results = [];
         for (const id in data) {
