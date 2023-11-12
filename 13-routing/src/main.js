03878d4377e27e9fafaf45f52cc99a62fb2e4c5f
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-12 14:16:09 +0100

    Not found route

--- 13-routing/src/main.js
+++ 13-routing/src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers';
+import NotFound from './components/nav/NotFound';
 
 const router = createRouter({
     history: createWebHistory(), // use built-in browser support to handle history
@@ -13,7 +14,8 @@ const router = createRouter({
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
         // setting props:true tells router that dynamic params (teamId) should be passed as props
-        { path: '/teams/:teamId', component: TeamMembers, props: true }
+        { path: '/teams/:teamId', component: TeamMembers, props: true },
+        { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active' // optional, will default to 'router-link-active'
 });
