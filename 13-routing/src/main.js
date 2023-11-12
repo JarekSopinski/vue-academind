28d7a5a72e8574e93461db63275cb3ad7a86c6e7
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-12 13:13:19 +0100

    Registering routes

--- 13-routing/src/main.js
+++ 13-routing/src/main.js
@@ -2,12 +2,19 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList';
+import UsersList from './components/users/UsersList';
 
 const router = createRouter({
     history: createWebHistory(), // use built-in browser support to handle history
-    routes: []
+    routes: [
+        { path: '/teams', component: TeamsList },
+        { path: '/users', component: UsersList }
+    ]
 });
 
-const app = createApp(App)
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
