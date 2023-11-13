764b8649d2152b289787bc72fb3cc53728186c2b
JarekSopinski <jarek.sopinski@gmail.com>
2023-11-13 06:50:06 +0100

    Confirmation before leaving route

--- 13-routing/src/components/users/UsersList.vue
+++ 13-routing/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,16 +14,32 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return { changesSaved: false }
+  },
   methods: {
     confirmInput() {
       // do something, than change route
       this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
     }
   },
   beforeRouteEnter(to, from, next) {
     // Component level navigation guard
-    console.log('UserList Cmp beforeRouteEnter');
+    console.log('UsersList Cmp beforeRouteEnter');
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    // Ask user to confirm navigation
+    console.log('UsersList Cmp beforeRouteLeave');
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You got unsaved changed!');
+      next(userWantsToLeave);
+    }
   }
 };
 </script>
