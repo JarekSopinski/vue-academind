fc95f45c099c176ce7a0441fa904fd2c1970a2eb
JarekSopinski <jarek.sopinski@gmail.com>
2024-01-03 07:21:01 +0100

    Example - adding more state

--- 15-vuex/src/App.vue
+++ 15-vuex/src/App.vue
@@ -1,24 +1,32 @@
 <template>
-  <base-container title="Vuex">
+  <base-container title="Vuex" v-if="userIsAuthenticated">
     <the-counter></the-counter>
     <favorite-value></favorite-value>
     <button @click="addTen">Add 10</button>
     <change-counter></change-counter>
   </base-container>
+
+  <base-container title="Auth">
+    <user-auth></user-auth>
+  </base-container>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
 import ChangeCounter from './components/ChangeCounter.vue';
 import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
     ChangeCounter,
-    FavoriteValue
+    FavoriteValue,
+    UserAuth
   },
   methods: {
     addTen() {
@@ -29,7 +37,10 @@ export default {
         value: 10
       })
     }
-  }
+  },
+  computed: {
+        ...mapGetters(['userIsAuthenticated'])
+    }
 };
 </script>
 
