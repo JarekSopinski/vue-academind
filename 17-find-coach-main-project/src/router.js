e9d3b69b876bb541e29411981375cfc3817e3e61
JarekSopinski <jarek.sopinski@gmail.com>
2024-01-13 19:24:10 +0100

    Coach Detail Component

--- 17-find-coach-main-project/src/router.js
+++ 17-find-coach-main-project/src/router.js
@@ -12,8 +12,8 @@ const router = createRouter({
     routes: [
         { path: '/', redirect: '/coaches' },
         { path: '/coaches', component: CoachesList },
-        { path: '/coaches/:id', component: CoachDetail, children: [
-            { path: '/contact', component: ContactCoach }, // coaches/c1/contact
+        { path: '/coaches/:id', component: CoachDetail, props: true, children: [
+            { path: 'contact', component: ContactCoach }, // coaches/c1/contact
         ] },
         { path: '/register', component: CoachRegistration },
         { path: '/requests', component: RequestsReceived },
