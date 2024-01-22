5f197c5bc832de1f9318e63ae2a175d7759e7bca
JarekSopinski <jarek.sopinski@gmail.com>
2024-01-22 07:49:40 +0100

    Added auth page

--- 17-find-coach-main-project/src/settings.js
+++ 17-find-coach-main-project/src/settings.js
@@ -1,1 +1,16 @@
 export const FIREBASE_URL = 'https://vue-find-coach-96f54-default-rtdb.europe-west1.firebasedatabase.app';
+
+/**
+ * Firebase rules backup
+ * 
+  "rules": {
+    "coaches": {
+       ".read": "true",
+      	".write": "auth != null"
+    },
+    "requests": {
+        ".read": "auth != null",
+      	".write": "true"
+    },
+  }
+ */
