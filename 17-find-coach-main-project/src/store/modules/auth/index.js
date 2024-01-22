d0e0ad9f390fad18839e522d14a866a52fe30964
JarekSopinski <jarek.sopinski@gmail.com>
2024-01-22 07:58:24 +0100

    Added auth module in Vuex

---
+++ 17-find-coach-main-project/src/store/modules/auth/index.js
@@ -0,0 +1,14 @@
+import mutation from './mutations';
+import actions from './actions';
+import getters from './getters';
+
+export default {
+    state() {
+        return {
+            userId: 'c3'
+        }
+    },
+    mutation,
+    actions,
+    getters
+}
