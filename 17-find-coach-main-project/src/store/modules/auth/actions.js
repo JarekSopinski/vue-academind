9ece834035854fd7de90e523ba5b98e14cc3bcc6
JarekSopinski <jarek.sopinski@gmail.com>
2024-01-26 06:31:14 +0100

    Refactored auth actions

--- 17-find-coach-main-project/src/store/modules/auth/actions.js
+++ 17-find-coach-main-project/src/store/modules/auth/actions.js
@@ -2,33 +2,28 @@ import { FIREBASE_SIGNUP_ENDPOINT, FIREBASE_LOGIN_ENDPOINT } from "../../../sett
 
 export default {
     async login(context, payload) {
-        const response = await fetch(`${FIREBASE_LOGIN_ENDPOINT}?key=${process.env.VUE_APP_FIREBASE_API_KEY}`, {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        });
-
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            console.error(responseData);
-            const error = new Error(responseData.message || 'Failed to authenticate.');
-            throw error;
-        }
-
-        console.log(responseData);
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
         });
     },
 
     async signup(context, payload) {
-        const response = await fetch(`${FIREBASE_SIGNUP_ENDPOINT}?key=${process.env.VUE_APP_FIREBASE_API_KEY}`, {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });
+    },
+
+    async auth(context, payload) {
+        const mode = payload.mode;
+
+        let url = FIREBASE_LOGIN_ENDPOINT;
+        if (mode === 'signup') {
+            url = FIREBASE_SIGNUP_ENDPOINT;
+        }
+
+        const response = await fetch(`${url}?key=${process.env.VUE_APP_FIREBASE_API_KEY}`, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -40,12 +35,10 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            console.error(responseData);
             const error = new Error(responseData.message || 'Failed to authenticate.');
             throw error;
         }
 
-        console.log(responseData);
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
