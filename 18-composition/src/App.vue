e010eeb91ecd1283790a910caebfd7837b8147de
JarekSopinski <jarek.sopinski@gmail.com>
2024-01-29 08:06:13 +0100

    Task - practive reactive data and functions

--- 18-composition/src/App.vue
+++ 18-composition/src/App.vue
@@ -1,46 +1,43 @@
 <template>
-  <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
-    <button @click="setNewUser">Change Age</button>
-  </section>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 v-if="courseData.goalVisibility">{{ courseData.goal }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggleGoalVisibility">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
 </template>
 
 <script setup>
 import { reactive } from 'vue';
 
-const user = reactive({
-  name: 'John',
-  age: 100
+// const courseGoal = ref('Finish the course!');
+// const goalIsVisible = ref(true);
+// const courseData = ref({
+//   goal: 'Finish the course!',
+//   goalVisibility: false
+// });
+const courseData = reactive({
+  goal: 'Finish the course!',
+  goalVisibility: false
 });
 
-const setNewUser = () => {
-  // Instead of 'methods' option, we can use normal functions.
-  user.name = 'Adam';
-  user.age = 101;
+const toggleGoalVisibility = () => {
+  // courseData.value.goalVisibility = !courseData.value.goalVisibility;
+  courseData.goalVisibility = !courseData.goalVisibility;
 }
-
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-}
-
 html {
   font-family: sans-serif;
 }
-
 body {
-  margin: 0;
-}
-
-.container {
-  margin: 3rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
+  margin: 3rem;
   text-align: center;
 }
 </style>
