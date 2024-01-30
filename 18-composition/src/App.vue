8ad4cc0ae6a6e6f24e416955b7ae9b0ce203e055
JarekSopinski <jarek.sopinski@gmail.com>
2024-01-30 07:49:51 +0100

    Replacing computed properties with 'computed'

--- 18-composition/src/App.vue
+++ 18-composition/src/App.vue
@@ -1,43 +1,52 @@
 <template>
-  <h2>My Course Goal</h2>
-  <!-- Task 1: Output your main course goal with help of the composition API -->
-  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3 v-if="courseData.goalVisibility">{{ courseData.goal }}</h3>
-  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button @click="toggleGoalVisibility">Toggle Goal</button>
-  <!-- Task 3: Manage data in three ways -->
-  <!-- => Separate refs -->
-  <!-- => Ref Object -->
-  <!-- => Reactive Object -->
-  <!-- Task 4: Also solve the assignment with the Options API -->
+  <section class="container">
+    <h2>{{ userName }}</h2>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName">
+      <input type="text" placeholder="Last Name" @input="setLastName">
+    </div>
+  </section>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { ref, computed } from 'vue';
 
-// const courseGoal = ref('Finish the course!');
-// const goalIsVisible = ref(true);
-// const courseData = ref({
-//   goal: 'Finish the course!',
-//   goalVisibility: false
-// });
-const courseData = reactive({
-  goal: 'Finish the course!',
-  goalVisibility: false
+const firstName = ref('');
+const lastName = ref('');
+
+const userName = computed(() => {
+  return firstName.value + ' ' + lastName.value;
 });
 
-const toggleGoalVisibility = () => {
-  // courseData.value.goalVisibility = !courseData.value.goalVisibility;
-  courseData.goalVisibility = !courseData.goalVisibility;
+const setFirstName = (event) => {
+  firstName.value = event.target.value;
 }
+
+const setLastName = (event) => {
+  lastName.value = event.target.value;
+}
+
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 html {
   font-family: sans-serif;
 }
+
 body {
-  margin: 3rem;
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
   text-align: center;
 }
 </style>
