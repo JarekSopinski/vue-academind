09665f2f5baa11e365d1072d6457e09c4e1ce100
JarekSopinski <jarek.sopinski@gmail.com>
2024-02-01 07:13:08 +0100

    Components and props with Composition

--- 18-composition/src/App.vue
+++ 18-composition/src/App.vue
@@ -1,18 +1,30 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
-    <h3>{{ userAge }}</h3>
+    <UserData
+      :firstName="firstName"
+      :lastName="lastName"
+      :age="age"
+    />
     <button @click="setAge">Set Age</button>
     <div>
-      <input type="text" placeholder="First Name" v-model="firstName">
-      <input type="text" placeholder="Last Name" ref="lastNameInput">
+      <input
+        type="text"
+        placeholder="First Name"
+        v-model="firstName"
+      >
+      <input
+        type="text"
+        placeholder="Last Name"
+        ref="lastNameInput"
+      >
       <button @click="setLastName">Set Last Name</button>
     </div>
   </section>
 </template>
 
 <script setup>
-import { ref, computed, watch } from 'vue';
+import { ref, watch } from 'vue';
+import UserData from './components/UserData.vue';
 
 const firstName = ref('');
 const lastName = ref('');
@@ -28,10 +40,6 @@ watch(userAge, (newValue, oldValue) => {
 watch([firstName, lastName], (newValues, oldValues) => {
   console.log('Old names ', oldValues);
   console.log('New names ', newValues);
-});
-
-const userName = computed(() => {
-  return firstName.value + ' ' + lastName.value;
 });
 
 const setAge = () => {
@@ -64,5 +72,4 @@ body {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
   text-align: center;
-}
-</style>
+}</style>
---
+++ 18-composition/src/components/UserData.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+</template>
+
+<script setup>
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+    firstName: String,
+    lastName: String,
+    age: Number
+});
+
+const userName = computed(() => {
+    return props.firstName + ' ' + props.lastName;
+});
+
+</script>
