f8c6acded8371cd6d5fd1baa074a799f54b52b6d
JarekSopinski <jarek.sopinski@gmail.com>
2024-02-01 07:43:18 +0100

    Lifecycle hooks with Composition

--- 18-composition/src/components/UserData.vue
+++ 18-composition/src/components/UserData.vue
@@ -4,7 +4,19 @@
 </template>
 
 <script setup>
-import { computed, defineProps, defineEmits, inject } from 'vue';
+import { 
+    computed, defineProps, defineEmits, inject, 
+    onBeforeMount, onMounted, 
+    onBeforeUpdate, onUpdated, 
+    onBeforeUnmount, onUnmounted
+} from 'vue';
+
+onBeforeMount(() => console.log('onBeforeMount'));
+onMounted(() => console.log('onMounted'));
+onBeforeUpdate(() => console.log('onBeforeUpdate'));
+onUpdated(() => console.log('onUpdated'));
+onBeforeUnmount(() => console.log('onBeforeUnmount'));
+onUnmounted(() => console.log('onUnmounted'));
 
 const props = defineProps({
     firstName: String,
