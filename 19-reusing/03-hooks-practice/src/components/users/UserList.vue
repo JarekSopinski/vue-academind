97fbb76a09386e20d2eb6ec646c4c2717e61c384
JarekSopinski <jarek.sopinski@gmail.com>
2024-02-03 09:36:23 +0100

    Created useSort custom hook

--- 19-reusing/03-hooks-practice/src/components/users/UserList.vue
+++ 19-reusing/03-hooks-practice/src/components/users/UserList.vue
@@ -19,8 +19,9 @@
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue';
 
+import useSort from '../../hooks/sort';
 import useSearch from '../../hooks/search';
 import UserItem from './UserItem.vue';
 
@@ -35,90 +36,16 @@ export default {
     const { users } = toRefs(props);
 
     const { enteredSearchTerm, availableItems, updateSearch } = useSearch(users, 'fullName');
-
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    const { sorting, displayedItems, sort } = useSort(availableItems, 'fullName');
 
     return {
       enteredSearchTerm,
       updateSearch,
-      displayedUsers,
+      displayedUsers: displayedItems,
       sorting,
       sort
     };
-  },
-  // data() {
-  //   return {
-  //     enteredSearchTerm: '',
-  //     activeSearchTerm: '',
-  //     sorting: null,
-  //   };
-  // },
-  // computed: {
-  //   availableUsers() {
-  //     let users = [];
-  //     if (this.activeSearchTerm) {
-  //       users = this.users.filter((usr) =>
-  //         usr.fullName.includes(this.activeSearchTerm)
-  //       );
-  //     } else if (this.users) {
-  //       users = this.users;
-  //     }
-  //     return users;
-  //   },
-  //   displayedUsers() {
-  //     if (!this.sorting) {
-  //       return this.availableUsers;
-  //     }
-  //     return this.availableUsers.slice().sort((u1, u2) => {
-  //       if (this.sorting === 'asc' && u1.fullName > u2.fullName) {
-  //         return 1;
-  //       } else if (this.sorting === 'asc') {
-  //         return -1;
-  //       } else if (this.sorting === 'desc' && u1.fullName > u2.fullName) {
-  //         return -1;
-  //       } else {
-  //         return 1;
-  //       }
-  //     });
-  //   },
-  // },
-  // methods: {
-  //   updateSearch(val) {
-  //     this.enteredSearchTerm = val;
-  //   },
-  //   sort(mode) {
-  //     this.sorting = mode;
-  //   },
-  // },
-  // watch: {
-  //   enteredSearchTerm(val) {
-  //     setTimeout(() => {
-  //       if (val === this.enteredSearchTerm) {
-  //         this.activeSearchTerm = val;
-  //       }
-  //     }, 300);
-  //   }
-  // },
+  }
 };
 </script>
 
