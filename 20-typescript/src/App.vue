aebe2c594a128fe9058f5d969e5b5b4f8fcca3d1
JarekSopinski <jarek.sopinski@gmail.com>
2024-02-03 10:59:16 +0100

    Vue components using TypeScript

--- 20-typescript/src/App.vue
+++ 20-typescript/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="app">
-    hello, hyrule
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">change name</button>
+    <button @click="changeAge(30)">change age</button>
   </div>
 </template>
 
@@ -9,7 +11,21 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-  components: {}
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+    }
+  }
 });
 </script>
 
